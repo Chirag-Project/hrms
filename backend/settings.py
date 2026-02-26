5c89d79c6a80120715d3c067042e53464df804e6
python01235 <cgdaitns@gmail.com>
2026-02-26 16:05:56 +0530

    adding db

--- backend/settings.py
+++ backend/settings.py
@@ -93,12 +93,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     )
 # }
 
-import dj_database_url
 import os
+import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
     )
 }
 
